38908106a4ac167325750aac35395e2d7ec61096
Patrycja-Bien <232039@student.uek.krakow.pl>
2024-10-26 17:59:58 +0200

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
-    public uint Size { get; set; } = 3;
+    private string description = "Unknown";
+    public required string Description { 
+        get { return description; }
+        init
+        {
+            description = value.Trim();
+            if (description.Length > 15)
+            {
+                description = description.Substring(0, 15).Trim();
+            }
+            if (description.Length < 3)
+            {
+                int missing = 3 - description.Length;
+                string hash = String.Concat(Enumerable.Repeat("#", missing));
+                description = $"{description}{hash}";
+            }
+            if (char.IsLower(description[0]))
+            {
+                description = char.ToUpper(description[0]) + description.Substring(1);
+            }
+        }
+    }
 
-    public void Info()
+    private uint size = 3;
+    public uint Size { get; set; }
+
+    public string Info
     {
-        Console.WriteLine($"{Description} <{Size}>");
+        get { return $"{Description} <{Size}>"; }
     }
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,56 @@ namespace Simulator;
 
 public class Creature
 {
-    public string? Name { get; set; }
-    public int Level { get; set; }
+    private string name = "Unknown";
+    public string Name
+    {
+        get { return name; }
+        init
+        {
+            name = value.Trim();
+            if (name.Length > 25)
+            {
+                name = name.Substring(0, 25).Trim();
+            }
+            if (name.Length < 3)
+            {
+                int missing = 3 - name.Length;
+                string hash = String.Concat(Enumerable.Repeat("#", missing));
+                name = $"{name}{hash}";
+            }
+            if (char.IsLower(name[0]))
+            {
+                name = char.ToUpper(name[0]) + name.Substring(1);
+            }
 
-    public Creature(string name, int level = 1)
+        }
+    }
+
+    private int level = 1;
+    public int Level 
+    { 
+        get {  return level; }
+        set
+        {
+            if (level == 1)
+            {
+                if (value < 1)
+                {
+                    level = 1;
+                }
+                else if (value > 10)
+                {
+                    level = 10;
+                }
+                else
+                {
+                    level = value;
+                }
+            }
+        }
+    }
+
+    public Creature(string name = "Unknown", int level = 1)
     {
         Name = name;
         Level = level;
@@ -25,6 +72,13 @@ public class Creature
     public void SayHi()
     {
         Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+    public void Upgrade()
+    {
+        if ( Level < 10 )
+        {
+            level++;
+        }
     }
     public string Info
     {
