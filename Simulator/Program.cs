3f30a446188334d3c59061f044b57f60decb0d48
Patrycja-Bien <232039@student.uek.krakow.pl>
2024-11-08 13:14:18 +0100

    Prostokąty

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,58 +5,50 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        static void Lab4a()
+        static void Lab5a()
         {
-            Console.WriteLine("HUNT TEST\n");
-            var o = new Orc() { Name = "Gorbag", Rage = 7 };
-            o.SayHi();
-            for (int i = 0; i < 10; i++)
+            Console.WriteLine("POINT CREATION AND METHODS TEST\n");
+            Point p = new(10, 25);
+            Console.WriteLine(p.Next(Direction.Right));          // (11, 25)
+            Console.WriteLine(p.NextDiagonal(Direction.Right));  // (11, 24)
+
+            Console.WriteLine("\nRECTANGLE CREATION TEST\n");
+            var r1 = new Rectangle(1, 2, 3, 4);
+            Console.WriteLine(r1); // (1, 2):(3, 4)
+            var r2 = new Rectangle(new Point(5, 6), new Point(7, 8));
+            Console.WriteLine(r2); // (5, 6):(7, 8)
+
+            Console.WriteLine("\nCOORDINATE ORDER TEST\n");
+            var r3 = new Rectangle(3, 4, 1, 2);
+            Console.WriteLine(r3); // (1, 2):(3, 4)
+            var r4 = new Rectangle(new Point(7,8), new Point(5,6));
+            Console.WriteLine(r4); // (5, 6):(7, 8)
+
+            Console.WriteLine("\nHANDLE EXCEPTION TEST\n");
+            try
             {
-                o.Hunt();
-                o.SayHi();
+                var r5 = new Rectangle(0, 1, 0, 2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Błąd tworzenia prostokąta r5: {ex.Message}");
+            }
+            try
+            {
+                var r6 = new Rectangle(new Point(1, 5), new Point(2, 5));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Błąd tworzenia prostokąta r6: {ex.Message}");
             }
 
-            Console.WriteLine("\nSING TEST\n");
-            var e = new Elf("Legolas", agility: 2);
-            e.SayHi();
-            for (int i = 0; i < 10; i++)
-            {
-                e.Sing();
-                e.SayHi();
-            }
+            var r7 = new Rectangle(0, 0, 6, 6);
 
-            Console.WriteLine("\nPOWER TEST\n");
-            Creature[] creatures = {
-        o,
-        e,
-        new Orc("Morgash", 3, 8),
-        new Elf("Elandor", 5, 3)
-    };
-            foreach (Creature creature in creatures)
-            {
-                Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
+            Console.WriteLine("\nCONTAINS METHOD TEST\n");
+            Console.WriteLine(r7.Contains(new Point(-1,-1))); 
+            Console.WriteLine(r7.Contains(new Point(6, 6)));
+            Console.WriteLine(r7.Contains(new Point(2,2)));
             }
-        }
-        Lab4a();
-        static void Lab4b()
-        {
-            object[] myObjects = {
-        new Animals() { Description = "dogs"},
-        new Birds { Description = "  eagles ", Size = 10 },
-        new Elf("e", 15, -3),
-        new Orc("morgash", 6, 4)
-    };
-            Console.WriteLine("\nMy objects:");
-            foreach (var o in myObjects) Console.WriteLine(o);
-            /*
-                My objects:
-                ANIMALS: Dogs <3>
-                BIRDS: Eagles (fly+) <10>
-                ELF: E## [10][0]
-                ORC: Morgash [6][4]
-            */
-        }
-        Lab4b();
-
+        Lab5a();
     }
 }
---
+++ Simulator/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+public class Rectangle
+{
+    public readonly int X1;
+    public readonly int Y1;
+    public readonly int X2;
+    public readonly int Y2;
+
+    public override string ToString() => $"({X1}, {Y1}):({X2}, {Y2})";
+
+    public Rectangle(int x1, int y1, int x2, int y2)
+    {
+        if ((x1 == x2) || (y1 == y2))
+        {
+            throw new ArgumentException("Punkty nie mogą być współliniowe");
+        }
+        X1 = (x1 < x2) ? x1 : x2;
+        X2 = (x1 < x2) ? x2 : x1;
+        
+        Y1 = (y1 < y2) ? y1 : y2;
+        Y2 = (y1 < y2) ? y2 : y1;
+    }
+    public Rectangle(Point p1, Point p2) : this(p1.X, p1.Y, p2.X, p2.Y)
+    {
+    }
+
+    public bool Contains(Point point)
+    {
+        if ((point.X > X1) && (point.X < X2))
+        {
+            if ((point.Y > Y1) && (point.Y < Y2))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
