ae8772b04ec3679d7015e42de57850366aa6fd16
Patrycja-Bien <232039@student.uek.krakow.pl>
2025-01-07 17:15:28 +0100

    Sort

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -7,7 +7,32 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Program");
+        var creatures = new List<Creature>
+        {
+            new Elf("A", level: 3, agility: 8),
+            new Orc("B", level: 7, rage: 6),
+            new Elf("C", level: 5, agility: 10),
+            new Orc("D", level: 1, rage: 8),
+            new Elf("E", level: 6, agility: 9),
+            new Orc("F", level: 10, rage: 5),
+            new Elf("G", level: 2, agility: 6),
+            new Orc("H", level: 8, rage: 7),
+        };
+
+        Console.WriteLine("Creatures before sorting:");
+        foreach (var creature in creatures)
+        {
+            Console.WriteLine($"{creature} Power: {creature.Power}");
+        }
+
+        creatures.Sort((a, b) => a.Power.CompareTo(b.Power));
+
+        Console.WriteLine("\nCreatures after sorting by Power:");
+        foreach (var creature in creatures)
+        {
+            Console.WriteLine($"{creature} Power: {creature.Power}");
+        }
+
 
     }
 }
